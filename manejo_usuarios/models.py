fa6781dd18ae4e8b9c34873aef8a0a29c37c1cd9
SERVYREX <codbryxp@gmail.com>
2026-06-03 17:15:15 -0400

    Ajuste menor a las tablas de usuarios

--- manejo_usuarios/models.py
+++ manejo_usuarios/models.py
@@ -2,28 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
-
-    rut = models.CharField(unique=True, primary_key=True, max_length=9) #se usa un charfield para el rut porque puede tener k
+    #se usa un charfield para el rut porque puede tener k
+    rut = models.CharField(unique=True, primary_key=True, max_length=9) 
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
     correo = models.EmailField(unique=True)
     fecha_nacimiento = models.DateField()
     
-class Paciente(models.Model):
+class Paciente(Usuario):
 
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
     telefono = models.IntegerField()
 
-class Personal(models.Model):
+class Personal(Usuario):
 
-    id_personal = models.IntegerField(unique=True, primary_key=True)
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    id_personal = models.IntegerField(unique=True)
     centro_vacunacion = models.ForeignKey('campana_vacunacion.CentroVacunacion', on_delete=models.SET_NULL, null=True)
 
-class Admin(models.Model):
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+class Admin(Usuario):
+    pass
     
-
+    
 class Historial(models.Model):
     
     usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
