b119bd0dc9157725fb403338a28382c0c95c5a69
SERVYREX <codbryxp@gmail.com>
2026-06-03 17:31:12 -0400

    Creacion de metodos necesarios para Campaña

--- campana_vacunacion/models.py
+++ campana_vacunacion/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -9,6 +10,54 @@ class Campaña(models.Model):
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     estado_campaña = models.BooleanField()
+
+    # Obtenemos la fecha de hoy, la comparamos con las fechas de inicio y fin y analizamos el estado de la campaña
+    def verificar_vigencia(self):
+        hoy = date.today()
+        if self.estado_campaña and self.fecha_inicio <= hoy <= self.fecha_fin:
+            return True
+        return False
+
+
+    def obtener_centros_disponibles(self):
+        # Retorna los centros disponibles. Por ahora retorna todos (COMPLETAR CUANDO SE IMPLEMENTE LA CLASE CENTRO)
+        return CentroVacunacion.objects.all()
+
+    def verificar_horarios_y_cupos(self, centro):
+        # Busca citas disponibles para esta campaña y centro (usando nombre_centro por ahora)
+        return Cita.objects.filter(
+            cita=self, # Filtramos aquellas citas pertenecientes a la campaña
+            ubicacion_cita=centro.nombre_centro,
+            estado_cita=True # Asumimos True como disponible 
+        )
+
+        #Recibimos la fecha y hora de la consulta y filtramos las citas disponibles
+    def verificar_disponibilidad_horario(self, centro, fecha, hora):
+        return Cita.objects.filter(
+            cita=self,
+            ubicacion_cita=centro.nombre_centro,
+            fecha_cita=fecha,
+            hora_cita=hora,
+            estado_cita=True
+        ).exists()
+
+    #Encontramos una cita disponible y la asociamos a un paciente
+    def agendar_cita(self, paciente, centro, fecha, hora):
+        cita_disponible = Cita.objects.filter(
+            cita=self,
+            ubicacion_cita=centro.nombre_centro,
+            fecha_cita=fecha,
+            hora_cita=hora,
+            estado_cita=True
+        ).first()
+
+        if cita_disponible:
+            cita_disponible.estado_cita = False # Marcamos como reservada
+            cita_disponible.paciente_citado = paciente
+            cita_disponible.save()
+            return True, cita_disponible
+        return False, None
+
 
 class Cita(models.Model):
 
